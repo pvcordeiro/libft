65556dc6635137125c27a4c9bba6f050d4f8d7d9
Paulo De Souza Cordeiro <paude-so@c2r8s4.42lisboa.com>
2024-10-30 13:49:42 +0000

    asd

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 11:20:46 by paude-so          #+#    #+#             */
-/*   Updated: 2024/10/30 11:29:11 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/10/30 13:49:33 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,20 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	i = 0;
 	if (dest > src)
 	{
-		while (n-- > 0)
+		while (n > 0)
+		{
 			((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
+			n--;
+		}
 		return (dest);
 	}
 	else
 	{
-		while (i++ < n)
+		while (i < n)
+		{
 			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
 	}
 	return (dest);
 }
