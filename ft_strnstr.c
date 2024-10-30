25a2bedf393dc9df2a54aa7d5f1933a425665251
Paulo De Souza Cordeiro <paude-so@c2r9s5.42lisboa.com>
2024-10-30 20:42:23 +0000

    Asda

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:24:51 by paude-so          #+#    #+#             */
-/*   Updated: 2024/10/25 12:38:07 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/10/30 20:41:27 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	big_i;
 	size_t	little_i;
 
-	if (!big)
-		return (NULL);
-	if (!little[0])
-		return ((char *)big);
 	big_i = 0;
 	while (big[big_i] && big_i < len)
 	{
