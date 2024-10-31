d4978bcd95f826f2096d9f764ccdc1418b399ad6
Paulo De Souza Cordeiro <paude-so@c2r8s4.42lisboa.com>
2024-10-31 13:25:22 +0000

    added nullcheck to strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:55:18 by paude-so          #+#    #+#             */
-/*   Updated: 2024/10/31 12:32:19 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/10/31 13:18:51 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*cat;
 	size_t	full_size;
 
+	if (!s1 || !s2)
+		return (NULL);
 	full_size = ft_strlen(s1) + ft_strlen(s2);
 	cat = (char *)malloc(full_size + 1);
 	if (!cat)
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -25,7 +25,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	i = 0;
 	while (s[i])
 	{
-		str[i] = (*f)(i, s[i]);
+		str[i] = f(i, s[i]);
 		i++;
 	}
 	return (str);
