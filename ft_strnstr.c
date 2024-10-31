1164af8bac3164a67362726d0323c3a3eaaeba72
Paulo De Souza Cordeiro <paude-so@c2r8s4.42lisboa.com>
2024-10-31 17:28:15 +0000

    refactored strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:24:51 by paude-so          #+#    #+#             */
-/*   Updated: 2024/10/30 20:44:19 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/10/31 17:27:47 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,17 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	big_i;
-	size_t	little_i;
+	size_t	l_len;
 
-	if (!little[0])
+	l_len = ft_strlen(little);
+	if (!l_len)
 		return ((char *)big);
-	big_i = 0;
-	while (big[big_i] && big_i < len)
+	while (*big && len >= l_len)
 	{
-		little_i = 0;
-		while (big[big_i + little_i] == little[little_i] && (big_i
-				+ little_i) < len)
-		{
-			if (!little[little_i + 1])
-				return ((char *)&big[big_i]);
-			little_i++;
-		}
-		big_i++;
+			if (!ft_strncmp(big, little, l_len))
+				return ((char *)big);
+			big++;
+			len--;
 	}
 	return (NULL);
 }
