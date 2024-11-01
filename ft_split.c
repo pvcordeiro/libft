5a67ad87129abadd8ab5e0f2bc4276fa8750ba99
Paulo Victor <pv.35@hotmail.com>
2024-11-01 11:54:04 +0000

    split v2

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 18:06:11 by paude-so          #+#    #+#             */
-/*   Updated: 2024/11/01 11:20:58 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/11/01 11:53:24 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	w_count(char const *s, char c)
+static size_t	ft_w_count(char const *s, char c)
 {
 	size_t	count;
 	size_t	word;
@@ -36,7 +36,7 @@ static size_t	w_count(char const *s, char c)
 	return (count);
 }
 
-static void	*panic_button(char **arr, size_t sub_i)
+static void	*ft_freearrs(char **arr, size_t sub_i)
 {
 	size_t	i;
 
@@ -50,7 +50,7 @@ static void	*panic_button(char **arr, size_t sub_i)
 	return (NULL);
 }
 
-static void	fill_words(char **str, const char *s, char c, size_t *i)
+static void	ft_fill_words(char **str, const char *s, char c, size_t *i)
 {
 	size_t	len;
 
@@ -64,51 +64,39 @@ static void	fill_words(char **str, const char *s, char c, size_t *i)
 	str[*i] = ft_substr(s, 0, len);
 	if (!str[*i])
 	{
-		panic_button(str, *i);
+		ft_freearrs(str, *i);
 		return ;
 	}
 	(*i)++;
-	fill_words(str, s + len, c, i);
+	ft_fill_words(str, s + len, c, i);
 }
-// static void	**null_handler(char **str, char const *s, char c)
-// {
-// 	if (!s || !*s)
-// 	{
-// 		str = (char **)ft_calloc(1, sizeof(char *));
-// 		if (!str)
-// 			return (NULL);
-// 		return (str);
-// 	}
-// 	if (!c)
-// 	{
-// 		str = (char **)ft_calloc(1, sizeof(char *));
-// 		if (!str)
-// 			return (NULL);
-// 		str[0] = ft_strdup(s);
-// 		return (str);
-// 	}	
-// }
+static char **ft_base_cases(char **str, char const *s, char c)
+{
+	if (!s || !*s)
+		return ((char **)ft_calloc(1, sizeof(char *)));
+	if (!c)
+	{
+		str = (char **)ft_calloc(2, sizeof(char *));
+		if (str)
+			str[0] = ft_strdup(s);
+		return (str);
+	}
+	return (NULL);
+}
 
 char	**ft_split(char const *s, char c)
 {
 	char	**str;
 	size_t	i;
 
-	i = 0;
-	if (!s || !*s)
-		return ((char **)ft_calloc(1, sizeof(char *)));
-	if (!c)
-	{
-		str = (char **)ft_calloc(1, sizeof(char *));
-		if (!str)
-			return (NULL);
-		str[0] = ft_strdup(s);
+	str = NULL;
+	str = ft_base_cases(str, s, c);
+	if (str)
 		return (str);
-	}
-	str = (char **)ft_calloc(w_count(s, c) + 1, sizeof(char *));
+	str = (char **)ft_calloc(ft_w_count(s, c) + 1, sizeof(char *));
 	if (!str)
 		return (NULL);
-	fill_words(str, s, c, &i);
-	str[i] = NULL;
+	i = 0;
+	ft_fill_words(str, s, c, &i);
 	return (str);
 }
