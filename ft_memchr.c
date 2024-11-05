84fe2a3c78d415ec5a8a122b9549eb2dff17555a
Paulo Victor <pv.35@hotmail.com>
2024-11-05 18:37:05 +0000

    cleaned up a few more

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:58:59 by paude-so          #+#    #+#             */
-/*   Updated: 2024/11/05 17:23:56 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/11/05 18:31:18 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,11 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	*str;
-
-	str = (unsigned char *)s;
 	while (n--)
 	{
-		if (*str == (unsigned char)c)
-			return (str);
-		str++;
+		if (*(unsigned char *)s == (unsigned char)c)
+			return ((unsigned char *)s);
+		s++;
 	}
 	return (NULL);
 }
