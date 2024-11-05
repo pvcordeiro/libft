32a592a1aa05684a079f18e181169c99e5619da6
Paulo Victor <pv.35@hotmail.com>
2024-11-05 17:24:21 +0000

    optimized memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:58:59 by paude-so          #+#    #+#             */
-/*   Updated: 2024/10/24 16:29:17 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/11/05 17:23:56 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,14 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
 	unsigned char	*str;
 
-	i = 0;
 	str = (unsigned char *)s;
-	while (i < n)
+	while (n--)
 	{
-		if (str[i] == (unsigned char)c)
-			return (str + i);
-		i++;
+		if (*str == (unsigned char)c)
+			return (str);
+		str++;
 	}
 	return (NULL);
 }
