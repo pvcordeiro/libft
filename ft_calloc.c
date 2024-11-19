6a624e6a7a49b95cecd361c25cac75240dc027d5
Paulo De Souza Cordeiro <paude-so@c2r2s11.42lisboa.com>
2024-11-19 15:19:12 +0000

    reverted calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:36:53 by paude-so          #+#    #+#             */
-/*   Updated: 2024/11/19 13:58:26 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/11/05 21:16:01 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,22 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	void	*alloc;
+	unsigned char	*alloc;
+	size_t			full_s;
 
-	if (size != 0 && nmemb != (size_t)-1 / size)
+	if (!nmemb || !size)
+	{
+		alloc = malloc(0);
+		if (!alloc)
+			return (NULL);
+		return (alloc);
+	}
+	full_s = nmemb * size;
+	if (nmemb != full_s / size)
 		return (NULL);
-	alloc = malloc(nmemb * size);
-	if(alloc)
-		ft_bzero(alloc, nmemb * size);
-	return (alloc);
+	alloc = malloc(full_s);
+	if (!alloc)
+		return (NULL);
+	ft_bzero(alloc, full_s);
+	return ((void *)alloc);
 }
